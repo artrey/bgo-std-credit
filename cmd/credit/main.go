09bae2e381e006226222f1472346a74deff8b01d
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2020-06-11 21:54:54 +0300

    Initial commit

---
+++ cmd/credit/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+import "github.com/artrey/bgo-std-credit/pkg/credit"
+
+func cents2rub(cents uint64) uint64 {
+	return uint64(math.Round(float64(cents) / 100))
+}
+
+func main() {
+	payment, overpay, total := credit.Calculate(1_000_000_00, 36, 20_000)
+	fmt.Printf("Monthly payment is %d rub\nOverpay is %d rub\nTotal sum is %d rub",
+		cents2rub(payment), cents2rub(overpay), cents2rub(total))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/artrey/bgo-std-credit
+
+go 1.14
