4ca743f1bdaaa2e5c71739bd3a2b5c341653ee74
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-01-09 13:13:30 +0300

    Reworked percent scaling

--- cmd/credit/main.go
+++ cmd/credit/main.go
@@ -11,7 +11,7 @@ func cents2rub(cents uint64) uint64 {
 }
 
 func main() {
-	payment, overpay, total := credit.Calculate(1_000_000_00, 36, 20_000)
+	payment, overpay, total := credit.Calculate(1_000_000_00, 36, 20_0)
 	fmt.Printf("Monthly payment is %d rub\nOverpay is %d rub\nTotal sum is %d rub",
 		cents2rub(payment), cents2rub(overpay), cents2rub(total))
 }
--- pkg/credit/credit.go
+++ pkg/credit/credit.go
@@ -1,13 +1,15 @@
 package credit
 
-import "math"
+import (
+	"math"
+)
 
-func Calculate(sum uint64, periods uint16, percent uint32) (monthlyPayment, overpay, totalSum uint64) {
-	// divide by months, percents scale (3 decimal places) and percents
-	// more accuracy variant: monthlyPercent := float64(percent) / 12 / 1000 / 100;
-	monthlyPercent := math.Round(float64(percent) * 10 / 12 / 100) / 10000
-	increaseCoef := math.Pow(1 + monthlyPercent, float64(periods))
-	k := monthlyPercent * increaseCoef / (increaseCoef - 1)
+// Calculate returns credit stats: monthly payment, overpay and total sum.
+func Calculate(sum uint64, periods uint16, percent uint16) (monthlyPayment, overpay, totalSum uint64) {
+	// divide by months, percents scale (1 decimal place) and percents
+	monthlyPercent := float64(percent) / 12 / 10 / 100
+	increaseFactor := math.Pow(1 + monthlyPercent, float64(periods))
+	k := monthlyPercent * increaseFactor / (increaseFactor - 1)
 	monthlyPayment = uint64(math.Round(k * float64(sum)))
 	totalSum = monthlyPayment * uint64(periods)
 	overpay = totalSum - sum
